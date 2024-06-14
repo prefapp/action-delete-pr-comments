427bc99a86c3a364422537718c32d918be6e127e
Jose Alvarez <jose.alvarez@prefapp.es>
2024-06-14 12:10:17 +0200

    feat: first release

---
+++ index.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    // `pr_number` input defined in action metadata file
+    const prNumber = core.getInput('pr_number');
+
+    // List the pull request comments in the issue
+    const octokit = github.getOctokit(this.ctx.token);
+
+    const { data: comments } = await octokit.rest.issues.listComments({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: prNumber
+    });
+
+
+    // Filter the comments
+    const filter = core.getInput('filter');
+    const filteredComments = filter
+        ? comments.filter(comment => comment.body.includes(filter))
+        : comments;
+
+    // Delete the comments
+    await Promise.all(filteredComments.map(comment => octokit.rest.issues.deleteComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        comment_id: comment.id
+    })));
+
+} catch (error) {
+    core.setFailed(error.message);
+}
